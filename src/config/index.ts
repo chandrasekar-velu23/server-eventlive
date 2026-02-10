9e514b47f3428a2cebb8ee13746016f36b6cd75d
Chandrasekar_velu <chandrasekarvelu23@gmail.com>
2026-02-10 16:22:24 +0530

    Refactor config for MongoDB URI and frontend URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,11 +5,8 @@ dotenv.config();
 export const config = {
     jwtSecret: process.env.JWT_SECRET as string,
     port: process.env.PORT || 5000,
-    // Add logic to choose the URI
-    mongoUri: process.env.USE_CLOUD_DB === 'true'
-        ? (process.env.DB_URI as string || process.env.MONGO_URI as string)
-        : (process.env.MONGO_URI as string || 'mongodb://127.0.0.1:27017/eventlive'),
+    mongoUri: process.env.DB_URI as string,
     env: process.env.NODE_ENV || 'development',
-    frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5173',
+    frontendUrl: process.env.FRONTEND_URL || 'https://eventliveclient.netlify.app/',
     googleClientId: process.env.GOOGLE_CLIENT_ID,
 };
